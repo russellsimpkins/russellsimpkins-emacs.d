462b7a9d9ac1ae0a214c04dea32889d0ae8f99e9
Russell Simpkins <russellsimpkins@super-duper-coder.local>
2011-02-18 08:07:58 -0500

    modified so that there are two methods and found a way to let the js-beautify-region allow it so that you don't have to mark a region. If you do not mark a region it will use the point to the end of line, else it will do the region

--- js-beautify.el
+++ js-beautify.el
@@ -7,16 +7,29 @@
        "Location of the js-beautify directory e.g. ~/software/js-beautify"
        :type '(string)
        :group 'data)
-(defun js-beautify ()
-  "Beautify a region of javascript using the code from jsbeautify.org"
+
+(defun js-beautify-region ()
+  "Beautify a region of javascript using the code from jsbeautify.org. If you don't have
+   a region selected, the code will go from the cursor to the end of the line."
   (interactive)
   (let ((orig-point (point)))
-    (unless (mark)
-      (mark-defun))
-    ;; 
+    (setq endm 0)
+    (if mark-active
+        (setq endm (mark))
+      (setq endm (point-at-eol))
+    )
     (setq command (concatenate 'string "java -jar " rhino-jar "/js.jar " beautify-dir  "/beautify-cl.js -i 4 -p -d " beautify-dir ))
     (shell-command-on-region (point)
-                             (mark)
+                             endm
                              command  nil t)
     (goto-char orig-point)))
+
+(defun js-beautify-buffer ()
+  "Beautify the buffer using the code from jsbeautify.org"
+  (interactive)
+  (let ((orig-point (point)))
+    (setq command (concatenate 'string "java -jar " rhino-jar "/js.jar " beautify-dir  "/beautify-cl.js -i 4 -p -d " beautify-dir ))
+    (shell-command-on-region (point-min) (point-max) command  nil t)
+    (goto-char orig-point)))
+
 (provide 'js-beautify)
