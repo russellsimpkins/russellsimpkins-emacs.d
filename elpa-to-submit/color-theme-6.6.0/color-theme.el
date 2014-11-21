e6abaa8119b23ddd39d6e4f5bdaaea20822b705a
Russell Simpkins <russell.simpkins@nytimes.com>
2014-11-21 12:10:15 -0500

    modified so it would work for me

--- elpa-to-submit/color-theme-6.6.0/color-theme.el
+++ elpa-to-submit/color-theme-6.6.0/color-theme.el
@@ -121,8 +121,7 @@ a plist.  In XEmacs, the alist is deprecated; a plist is used instead."
 	 plist)
 	((not (symbolp (car plist)))
 	 (error "Wrong type argument: plist, %S" plist))
-	((featurep 'xemacs)
-	 (plist-to-alist plist)))); XEmacs only
+
 
 ;; Customization
 
